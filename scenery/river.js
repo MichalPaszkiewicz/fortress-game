d9b3d04e26f13c6f0ef98c8c6acc72c268a78e49
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-03-11 18:37:35 +0000

    starting on river

---
+++ scenery/river.js
@@ -0,0 +1,20 @@
+var River = function(x, y, m, c){
+	this.x = x;
+	this.y = y;
+	this.m = m;
+	this.c = c;
+	
+	this.sections = function()
+	{
+		var intersectX = {x: -this.c / this.m, y: 0;
+		var intersectY = {x: 0, y: y};
+		var intersect = {x: 0, y: 0};
+		
+		return [
+			//river
+			[{type:"set",x:x,y:y},]
+		];
+	}
+	
+	return this;
+}
